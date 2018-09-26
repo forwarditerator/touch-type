a9903e916b954ad1948c3858aa18ec48a446fd0f
Carlos Lemos <carloslemos@Mac.local>
2018-09-26 12:19:25 -0400

    evaluating time to add a char and refresh the screen

--- type.cpp
+++ type.cpp
@@ -1,7 +1,21 @@
 #include <ncurses.h>
+#include <chrono>
+#include <vector>
+
 int main() {
 	initscr();
-	refresh();
-	getch();
+    noecho();
+    std::vector<int> time;
+    for (int i = 0; i < 1000; ++i) {
+        auto start = std::chrono::steady_clock::now();
+        addch('a');
+        refresh();
+        auto end = std::chrono::steady_clock::now();
+        time.push_back(std::chrono::duration_cast<std::chrono::microseconds>(end-start).count());
+    }
+    clear();
+    for (auto t : time)
+        printw("%4d", t);
+    getch();
 	endwin();
 }
